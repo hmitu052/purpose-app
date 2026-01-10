50c3779b9fb9ea18e6a5967577aa932b53aa06f8
hmitu052 <hanamitu0106@gmail.com>
2026-01-10 18:40:07 +0900

    Render対応:PORT設定

--- server.js
+++ server.js
@@ -78,7 +78,8 @@ app.get("/logs/:userId", (req, res) => {
   );
 });
 
-// ===== サーバ起動 =====
-app.listen(3000, () => {
-  console.log("http://localhost:3000 で起動中");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
